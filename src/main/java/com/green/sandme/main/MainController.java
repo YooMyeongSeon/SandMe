7a189acae4637e30e222c4d4a1d2d547ef4fdaab
YooMyeongSeon <s.cnelly@hotmail.com>
2022-07-19 18:02:21 +0900

    2022.07.19 commit

--- src/main/java/com/green/sandme/main/MainController.java
+++ src/main/java/com/green/sandme/main/MainController.java
@@ -6,7 +6,6 @@ import org.apache.ibatis.session.SqlSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.green.sandme.main.vo.BestVo;
--- src/main/java/com/green/sandme/order/OrderController.java
+++ src/main/java/com/green/sandme/order/OrderController.java
@@ -1,11 +1,17 @@
 package com.green.sandme.order;
 
+import java.io.PrintWriter;
+
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.google.gson.Gson;
 
 @Controller
 public class OrderController {
@@ -16,24 +22,46 @@ public class OrderController {
 		if (order == null || order.equals("home")) {
 			model.addAttribute("order", "home");
 		} else {
-			model.addAttribute("order", "pickUp");
+			model.addAttribute("order", order);
 		}
 		
+		model.addAttribute("chapter", "chapter01");
 		return "order";
 	}
 	
 	@PostMapping("/order")
-	public String OderChapter01(HttpServletRequest request, Model model) {
+	public String OderChapter(HttpServletRequest request, Model model) {
 		String order = request.getParameter("order");
-		String address = request.getParameter("address");
-		
-		if (order == null || order.equals("home")) {
-			model.addAttribute("order", "home");
-		} else {
-			model.addAttribute("order", "pickUp");
+		String chapter = request.getParameter("chapter");
+
+		if (chapter.equals("chapter01")) {
+			String address = request.getParameter("address");
+			model.addAttribute("address", address);
+			model.addAttribute("chapter", "chapter02");
+		} else if (chapter.equals("chapter02")) {
+			model.addAttribute("chapter", "chapter03");
+		} else if (chapter.equals("chapter03")) {
+			model.addAttribute("chapter", "chapter04");
 		}
 		
-		model.addAttribute("address", address);
+		model.addAttribute("order", order);
 		return "order";
+	}
+	
+	@PostMapping("/order/shop")
+	public void OderShop(@RequestBody String address, HttpServletResponse response) throws Exception {
+		response.setCharacterEncoding("UTF-8");
+
+		if (address.contains("정자동")) {
+			
+		} else {
+			
+		}
+		
+		Gson gson = new Gson();
+		
+		String data = gson.toJson("데이터");
+		PrintWriter out = response.getWriter();
+		out.print(data);
 	}
 }
