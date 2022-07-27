1bd1357882a1bb47b35b69e45221d4e1203adc28
kingminjung <kmj990304@gmail.com>
2022-07-27 17:27:34 +0900

    2022.07.27 commit

--- src/main/java/com/green/sandme/SandmeApplication.java
+++ src/main/java/com/green/sandme/SandmeApplication.java
@@ -9,8 +9,15 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.core.io.Resource;
 import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
-
+@EnableWebSecurity //시큐리티 필터 작동  
 @SpringBootApplication
 public class SandmeApplication {
 
@@ -28,4 +35,49 @@ public class SandmeApplication {
         
         return sessionFactory.getObject();
     }
+	
+	//비밀번호 해시처리(시큐리티는 비밀번호를 해시처리하지 않으면 가입불가)  
+	@Bean 
+	BCryptPasswordEncoder encoderPassword() { 
+		return new BCryptPasswordEncoder(); 
+	}
+	  
+	@Bean 
+	SecurityFilterChain filterChain(HttpSecurity http) throws Exception { 
+		http.cors() //교차 출처 리소스 공유
+			.configurationSource(corsConfigurationSource())
+			.and().csrf().disable() //취약점 막기
+			.authorizeRequests() //페이지 권한 설정
+			.anyRequest().permitAll() //특정 권한 사용자만 접근가능한 리소스를 설정 그 외 나머지는 인증후 접근 가능
+			.and()  //로그인 페이지와 기타 로그인 처리 및 성공 실패 처리를 사용 
+			.formLogin() //따로 만든 로그인 페이지 사용할때 미설정시 시큐리티가 기본으로 제공하는 페이지 
+			.loginPage("/login") 
+			.usernameParameter("memberEmail") 
+			.passwordParameter("memberPwd")
+			.loginProcessingUrl("/loginSuccess") //로그인폼 액션값과 일치
+			.defaultSuccessUrl("/") //로그인이 성공했을 때 이동되는 페이지
+			.failureUrl("/login")
+			.permitAll()
+			.and() 
+			.logout() 
+			.logoutUrl("/logout") //로그아웃 매퍼
+			.logoutSuccessUrl("/") //로그아웃 성공시 이동되는 페이지
+			.invalidateHttpSession(true); //Http세션 초기화
+		return http.build();
+	}
+
+	@Bean
+	CorsConfigurationSource corsConfigurationSource() {
+		CorsConfiguration configuration = new CorsConfiguration();
+		
+		configuration.addAllowedOrigin("*");
+		configuration.addAllowedHeader("*");
+		configuration.addAllowedMethod("*");
+		configuration.setAllowCredentials(true);
+		
+		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+		source.registerCorsConfiguration("/**", configuration);
+		
+		return source;
+	}
 }
---
+++ src/main/java/com/green/sandme/member/vo/memberVo.java
@@ -0,0 +1,76 @@
+package com.green.sandme.member.vo;
+
+import java.util.Collection;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+public class memberVo implements UserDetails {
+	private static final long serialVersionUID = 1L;
+	
+	private int memberNum;
+	private String memberEmail;
+	private String memberPwd;
+	private String memberTel;
+	private String memberName;
+	
+	public int getMemberNum() {
+		return memberNum;
+	}
+	public void setMemberNum(int memberNum) {
+		this.memberNum = memberNum;
+	}
+	public String getMemberEmail() {
+		return memberEmail;
+	}
+	public void setMemberEmail(String memberEmail) {
+		this.memberEmail = memberEmail;
+	}
+	public String getMemberPwd() {
+		return memberPwd;
+	}
+	public void setMemberPwd(String memberPwd) {
+		this.memberPwd = memberPwd;
+	}
+	public String getMemberTel() {
+		return memberTel;
+	}
+	public void setMemberTel(String memberTel) {
+		this.memberTel = memberTel;
+	}
+	public String getMemberName() {
+		return memberName;
+	}
+	public void setMemberName(String memberName) {
+		this.memberName = memberName;
+	}
+	
+	@Override
+	public Collection<? extends GrantedAuthority> getAuthorities() {
+		return null;
+	}
+	@Override
+	public String getPassword() {
+		return this.memberPwd;
+	}
+	@Override
+	public String getUsername() {
+		return this.memberEmail;
+	}
+	@Override
+	public boolean isAccountNonExpired() {
+		return true;
+	}
+	@Override
+	public boolean isAccountNonLocked() {
+		return true;
+	}
+	@Override
+	public boolean isCredentialsNonExpired() {
+		return true;
+	}
+	@Override
+	public boolean isEnabled() {
+		return true;
+	}
+}
